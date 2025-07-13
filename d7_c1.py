6b1dd49e78741b992b42c7d4d70ba074a7526e1d
itzz-aadii <orewaadii@gmail.com>
2025-07-13 11:29:54 +0530

    Input learnt

---
+++ d7_c1.py
@@ -0,0 +1,6 @@
+new = input("Input str: ")
+print("The input was: ", new)
+num1 = input("Enter 1st: ")
+num2 = input("Enter 2nd: ")
+print("Str output: ", num1+num2)
+print("Output: ", int(num1)+int(num2))
