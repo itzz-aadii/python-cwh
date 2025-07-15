dd8d6c39edf2f412ee31baad21e4002e382d02df
itzz-aadii <orewaadii@gmail.com>
2025-07-15 20:23:29 +0530

    #day01-10

--- d06_ex.py
+++ d06_ex.py
@@ -1,10 +1,10 @@
-#day1-10
+# day1-10
 num1 = 100
 num2 = 4
-print("sum", num1+num2)
-print("sub", num1-num2)
-print("multiply", num1*num2)
-print("divide", num1/num2)
-print("floor division", num1//num2)
+print("sum", num1 + num2)
+print("sub", num1 - num2)
+print("multiply", num1 * num2)
+print("divide", num1 / num2)
+print("floor division", num1 // num2)
 print("modulus", num1 % num2)
 print("Expo", num1**num2)
